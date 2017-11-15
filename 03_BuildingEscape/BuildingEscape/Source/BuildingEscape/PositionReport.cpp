29f749d2192e1594fc5a492fecee19b2e79536b3
Oli-PC\Oli <olistiel@gmail.com>
2017-11-15 18:22:49 +0000

    Room Geometry

--- 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
+#include "Modules/ModuleManager.h"
 
 
 // Sets default values for this component's properties
@@ -8,7 +10,6 @@ UPositionReport::UPositionReport()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
@@ -20,7 +21,10 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	FString ObjectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
